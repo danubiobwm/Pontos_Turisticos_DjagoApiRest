4e24c1c03c4e010d5d225c6b1d06241391eb1467
danubiobwm <danubio.bwm@gmail.com>
2020-09-24 23:11:23 -0300

    endpoint avançados

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
@@ -12,3 +13,23 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         return Response({'teste': 123})
+
+    def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data['nome']})
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, resquest):
+        pass
