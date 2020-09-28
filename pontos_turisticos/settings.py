ef0b8667d606e0981de44e9321d254ec4b3182a0
danubiobwm <danubio.bwm@gmail.com>
2020-09-27 23:13:21 -0300

    filters

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -28,7 +28,8 @@ INSTALLED_APPS = [
     'atracoes',
     'comentarios',
     'avaliacoes',
-    'enderecos'
+    'enderecos',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -109,4 +110,8 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = 'imagens'
 
-MEDIA_URL='/media/'
+MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
